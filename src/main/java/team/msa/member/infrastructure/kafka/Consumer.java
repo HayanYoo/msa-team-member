9f171d5beb72ffcd6136d526b743ccb6104b1c7d
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-04-29 01:13:53 +0900

    feat kafka Consumer

---
+++ src/main/java/team/msa/member/infrastructure/kafka/Consumer.java
@@ -0,0 +1,20 @@
+package team.msa.member.infrastructure.kafka;
+
+
+import org.apache.kafka.clients.producer.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class Consumer {
+    private final Logger logger = (Logger) LoggerFactory.getLogger(Producer.class);
+
+    @KafkaListener(topics = "kafka_practice", groupId = "group_id_kafka")
+    public void consume(String message) throws IOException {
+        logger.info(String.format("#### -> Consumed message -> %s", message));
+    }
+}
